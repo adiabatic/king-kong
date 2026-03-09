d13ce816e9adbb1b65ce63fd4c19e3007829b60c
Nathan Galt <adiabatic@users.noreply.github.com>
2026-03-09 01:51:45 -0700

    Python 3-ize

--- layoutlint.py
+++ layoutlint.py
@@ -4,7 +4,7 @@
 import re
 # Yes, you're thinking “Why is this crazy person using regular expressions to parse an XML file? He should be using xml.dom.minidom.” That was the first thing I tried. Trouble is, it chokes on Ukelele's malformed “<?XML” and on esoteric constructions like “&#xdead;”. Yeah. So that’s why I’m using regular expressions.
 
-letters = set(u'')
+letters = set('')
 
 r = re.compile(r'\s*<key code="(\d*)" output="(.*)"/>')
 
@@ -18,13 +18,12 @@ for line in lines:
 	m = r.match(line)
 	if m and m.group(2):
 		letter = m.group(2)
-		print letter
-		letter = unicode(letter, "UTF-8")
+		print(letter)
 		usedLetters.add(letter)
 	else:
-		print ".",
+		print(".", end="")
 
-print
+print()
 unusedLetters = letters - usedLetters
-print unusedLetters
-print "Missing {} of {} letters".format(len(unusedLetters), len(letters))
+print(unusedLetters)
+print(f"Missing {len(unusedLetters)} of {len(letters)} letters")
